f7b8f53922fba3d6b6d5b4dd3a4f1334653ba5b9
varun__upadhyay <varun.802.vu@gmail.com>
2024-01-20 14:50:00 +0530

    conditional things

--- index.js
+++ index.js
@@ -1,15 +1,55 @@
 const GITHUB_API_USERINFO = "https://api.github.com/users/";
-const userName = "VarunUpadhyay802";
-const full_name = document.getElementById("full_name")
-const Following_count = document.getElementById("Following_count")
+
+const full_name = document.getElementById("full_name");
+const Following_count = document.getElementById("Following_count");
+const Follower_count = document.getElementById("Follower_count");
+const user_image = document.getElementById("user_image");
+const user_email = document.getElementById("user_email");
+const user_bio = document.getElementById("user_bio");
+const twitter_username = document.getElementById("twitter_username");
+const github_link = document.getElementById("github_link");
+const searchInput = document.getElementById("searchInput")
+const searchButton = document.querySelector(".searchButton");
+const resultDiv = document.getElementById("resultDiv");
+const need_to_search = document.getElementById("need_to_search")
+// const validateUser=(userName)=>{
+//     fetch(GITHUB_API_USERINFO + userName).then((res)=>{
+//         if(res.status===200){
+//             return true;
+//         }
+//         else
+//         return false
+//     })
+// }
+searchButton.addEventListener("click", (e) => {
 
 
-fetch(GITHUB_API_USERINFO + userName).then(response => response.json()).then(data => {
+    console.log("hey");
+    const searchValue = searchInput.value;
+    const userName = searchValue;
+    fetch(GITHUB_API_USERINFO + userName)
+        .then(response => response.json())
+        .then(data => {
+            resultDiv.style.display = "block";
+            need_to_search.style.display = "none"
 
-    full_name.textContent = data.name
-    Following_count.textContent = data.following
-    console.log(data);
-    console.log("ID:", data.id);
-    console.log("Username:", data.login);
-    console.log("Public Repositories:", data.public_repos);
-}).catch(error => console.error("Error:", error))
+            // Update HTML elements with user information
+            full_name.textContent = data.name || "N/A";
+            Following_count.textContent = data.following || "N/A";
+            Follower_count.textContent = data.followers || "N/A";
+            user_image.src = data.avatar_url || "default_user.jpg";
+
+            // Additional Information
+            user_email.textContent = data.email || "N/A";
+            user_bio.textContent = data.bio || "N/A";
+            twitter_username.textContent = data.twitter_username || "N/A";
+            github_link.href = data.html_url;
+            github_link.textContent = "link"
+
+
+        })
+        .catch(error => console.error("Error:", error));
+    e.preventDefault();
+
+}
+)
